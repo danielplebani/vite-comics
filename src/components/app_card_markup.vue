f1c580e3a95e4cfc4d083c86c9b6abfc0d5fb198
danielplebani <danielpleba03@gmail.com>
2023-09-23 14:11:24 +0200

    card markup with v-for

--- src/components/app_card_markup.vue
+++ src/components/app_card_markup.vue
@@ -1,50 +1,51 @@
 <script>
-  export default{
-    name: 'app_main_card_markup',
+export default{
+  name: 'app_card_markup',
 
-    data(){
+  data(){
         return{
             cards: [
                 {
                     name: 'DIGITAL COMICS',
-                    img: '../assets/img/buy-comics-digital-comics.png'
+                    img: 'src/assets/img/buy-comics-digital-comics.png'
                 },
                 {
                     name: 'DC MERCHANDISE',
-                    img: '../assets/img/buy-comics-merchandise.png'
+                    img: 'src/assets/img/buy-comics-merchandise.png'
                 },
                 {
                     name: 'SUBSCRIPTION',
-                    img: '../assets/img/buy-comics-subscriptions.png'
+                    img: 'src/assets/img/buy-comics-subscriptions.png'
                 },
                 {
                     name: 'COMIC SHOP LOCATOR',
-                    img: '../assets/img/buy-comics-shop-locator.png'
+                    img: 'src/assets/img/buy-comics-shop-locator.png'
                 },
                 {
                     name: 'DC POWER VISA',
-                    img: '../assets/img/buy-dc-power-visa.svg'
-                },
+                    img: 'src/assets/img/buy-dc-power-visa.svg'
+                }
             ]
         }
     }
-  }
+  
+}
 </script>
 
 <template>
-    <div class="d-flex align-items-center">
-        <div class="col d-flex justify-content-center" v-for="card in cards">
-          <img :src="card.img" class="me-2">
-          <span>{{card.name}}</span>
-        </div>
+   <div class="d-flex align-items-center">
+      <div class="col d-flex justify-content-center" v-for="card in cards">
+        <img :src="card.img" class="me-2">
+        <span>{{card.name}}</span>
+      </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
+
 img{
   width: 40px;
 }
-
 span{
   font-family: 'Roboto Condensed', sans-serif;
   color: white;
--- src/components/app_main_card.vue
+++ src/components/app_main_card.vue
@@ -1,17 +1,18 @@
 <script>
-  import app_main_card_markup from './app_main_card_markup.vue';
 
-  export default{
-    name: 'app_main_card',
+import app_card_markup from './app_card_markup.vue';
 
-    components:{
-      app_main_card_markup
-    }
+export default{
+  name: 'app_main_card',
+
+  components:{
+    app_card_markup
   }
+}
 </script>
 
 <template>
-  <app_main_card_markup></app_main_card_markup>
+  <app_card_markup></app_card_markup>
 </template>
 
 <style lang="scss" scoped>
