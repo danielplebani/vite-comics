44f1281eb218c60edd42c5aaedd7ec251d1a5a60
danielplebani <danielpleba03@gmail.com>
2023-09-23 14:35:54 +0200

    dynamic main_body

---
+++ src/components/app_body_list.vue
@@ -0,0 +1,44 @@
+<script>
+  export default{
+    name: 'app_body_list',
+
+    data(){
+        return{
+            DCcomics:['Characters','Comics','Movies','Tv','Games','Videos','News',],
+            SHOP:['Shop DC','Shop DC collecctive'],
+            DC:['Terms of use','Privacy police (news)','Movies','Tv','Games','Videos','News','News','News','News','News'],
+            SITES:['DC','Mag magazine','DC Kids','DC university','DC power']
+        }
+    }
+  }
+</script>
+
+<template>
+    <div class="col-3 d-flex flex-column">
+            <span class="my-2"><strong>DC COMICS</strong></span>
+            <a href="#" v-for="element in DCcomics">{{element}}</a>
+
+            <span class="my-2"><strong>SHOP</strong></span>
+            <a href="#" v-for="element in SHOP">{{element}}</a>
+    </div>
+
+    <div class="col-3 d-flex flex-column">
+                <span class="my-2"><strong>DC</strong></span>
+            <a href="#" v-for="element in DC">{{element}}</a>
+                
+    </div>
+
+    <div class="col-3 d-flex flex-column">
+                <span class="my-2"><strong>SITES</strong></span>
+                <a href="#" v-for="element in SITES">{{element}}</a>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+a{
+    color: gray;
+    text-decoration: none;
+    font-size: x-small;
+    margin-bottom: 3px;
+}
+</style>
--- src/components/app_main_body.vue
+++ src/components/app_main_body.vue
@@ -1,48 +1,20 @@
 <script>
+
+  import app_body_list from './app_body_list.vue';
+
   export default{
-    name: 'app_main_body'
+    name: 'app_main_body',
+
+    components:{
+        app_body_list
+    }
   }
 </script>
 
 <template>
     <div class="d-flex text-white">
         <div class="col d-flex justify-content-start py-3">
-            <div class="col-3 d-flex flex-column">
-                <span class="my-2"><strong>DC COMICS</strong></span>
-                <a href="#">Characters</a>
-                <a href="#">Comics</a>
-                <a href="#">Movies</a>
-                <a href="#">Tv</a>
-                <a href="#">Games</a>
-                <a href="#">Videos</a>
-                <a href="#">News</a>
-
-                <span class="my-2"><strong>SHOP</strong></span>
-                <a href="#">Shop DC</a>
-                <a href="#">Shop DC collectibles</a>
-            </div>
-            <div class="col-3 d-flex flex-column">
-                <span class="my-2"><strong>DC</strong></span>
-                <a href="#">Terms of use</a>
-                <a href="#">Privacy police (news)</a>
-                <a href="#">Movies</a>
-                <a href="#">Tv</a>
-                <a href="#">Games</a>
-                <a href="#">Videos</a>
-                <a href="#">News</a>
-                <a href="#">News</a>
-                <a href="#">News</a>
-                <a href="#">News</a>
-                <a href="#">News</a>
-            </div>
-            <div class="col-3 d-flex flex-column">
-                <span class="my-2"><strong>SITES</strong></span>
-                <a href="#">DC</a>
-                <a href="#">Mag magazine</a>
-                <a href="#">DC Kids</a>
-                <a href="#">DC university</a>
-                <a href="#">DC power</a>
-            </div>
+            <app_body_list></app_body_list>
         </div>
 
         <div class="col logo">
@@ -51,13 +23,6 @@
 </template>
 
 <style lang="scss" scoped>
-a{
-    color: gray;
-    text-decoration: none;
-    font-size: x-small;
-    margin-bottom: 3px;
-}
-
 .logo{
     background-image: url(../assets/img/dc-logo-bg.png);
     background-repeat: no-repeat;
